00f49620457b49402cb6cba91471b9a561bdfcd2
Kal <kal9001@hotmail.com>
2016-03-11 06:12:10 +0000

    Update MBR.asm

--- x86/Kal_OS/dev-0-001/boot/MBR.asm
+++ x86/Kal_OS/dev-0-001/boot/MBR.asm
@@ -1,3 +1,29 @@
+;Kal_OS master boot record. Stage 1 boot loader.
+;  Web: http://home.kal9001.co.uk  Email: kal@kal9001.co.uk
+;
+;  Copyright (C) 2016 Kal9001
+;
+;  This program is free software: you can redistribute it and/or modify
+;  it under the terms of the GNU General Public License as published by
+;  the Free Software Foundation, either version 3 of the License, or
+;  (at your option) any later version.
+;
+;  This program is distributed in the hope that it will be useful,
+;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;  GNU General Public License for more details.
+;
+;  You should have received a copy of the GNU General Public License
+;  called GPLv3.txt in the root directory where this source file is located.
+;  If not, see <http://www.gnu.org/licenses/>.
+;
+;  Features:
+;  512B 1st stage boot loader, Pulls a further 15 sectors from the disk and writes them directly after the first.
+;  These sectors form the stage 2 boot loader which is in a seperate file.
+;  *note right now we dont care about a partition table...
+;  **note this binary must start on the first sector of the disk.
+;  
+
 [BITS 16]
 org 0x7c00
 
