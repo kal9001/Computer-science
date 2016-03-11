8b0c9eea06d61b90bddf5f2864c957af3163000c
Kal <kal9001@hotmail.com>
2016-03-11 05:26:33 +0000

    Update MBR.asm

--- x86/Kal_OS/dev-0-001/boot/MBR.asm
+++ x86/Kal_OS/dev-0-001/boot/MBR.asm
@@ -1,52 +1,25 @@
-;  Kal_OS master boot record. Stage 1 boot loader.
-;  Web: http://home.kal9001.co.uk  Email: kal@kal9001.co.uk
-;
-;  Copyright (C) 2016 Kal9001
-;
-;  This program is free software: you can redistribute it and/or modify
-;  it under the terms of the GNU General Public License as published by
-;  the Free Software Foundation, either version 3 of the License, or
-;  (at your option) any later version.
-;
-;  This program is distributed in the hope that it will be useful,
-;  but WITHOUT ANY WARRANTY; without even the implied warranty of
-;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;  GNU General Public License for more details.
-;
-;  You should have received a copy of the GNU General Public License
-;  called GPLv3.txt in the root directory where this source file is located.
-;  If not, see <http://www.gnu.org/licenses/>.
-;
-;  Features:
-;  512B 1st stage boot loader, Pulls a further 15 sectors from the disk and writes them directly after the first.
-;  These sectors form the stage 2 boot loader which is in a seperate file.
-;  *note right now we dont care about a partition table...
-;  **note this binary must start on the first sector of the disk.
-;  
-;
-
-bits 16
-[org 0x7c00]
+[BITS 16]
+org 0x7c00
 
 ;set up the temp stack to use in 16bit mode just below the upper memory area
-xor ax, ax        	;set ax to zero
+xor ax, ax		;set ax to zero
 mov es, ax		;set buffer segment to zero for later
-mov ds, ax      	;set data segment to start of ram
-add ax, 0x7000    	;set ax to stack segment
+mov ds, ax		;set data segment to start of ram
+add ax, 0x7000		;set ax to stack segment
 mov ss, ax		;set stack segment to lowest address
-mov sp, 0xfffe  	;create stack just below the 
-mov bp, sp	        ;set frame pointer to match
+mov sp, 0xfffe		;create stack just below the 
+mov bp, sp		;set frame pointer to match
 
-mov [0x7dfe], dx	;preserve the boot drive number over the MBR ID incase we need it later
+mov [0x7dfe], dx	;preserve the boot drive number
 
-;This pulls a further 15 sectors from the disk.
+
 mov ah, 0x02 		;BIOS disk read sectors
 ;dl already set		;from which drive
 mov ch, 0x00 		;(C) track/cylinder   
 ;dh already set		;(H) R/W head
 mov cl, 0x02 		;(S) start sector
 mov al, 0x0f 		;number of sectors to read
-
+			
 ;es already set		;segment for buffer		
 mov bx, 0x7e00		;set offset for buffer
 int 0x13		;execute disk read
@@ -74,18 +47,82 @@ new_line:;Prints line line feed and carriage return
    call print_string	;call to print the newline chars
    pop si		;restore si
    ret			;return from function
+
+A20_check:;checks if the A20 line is enabled or not
+   ;fs is set by A20_enable to 0xffff
+   ;ds is already set for the bootloadr at 0x0000.
+
+   mov di, 0x0500	;for low memory offset
+   mov si, 0x0510	;for high memory offset
+ 
+   mov al, byte [fs:si]	;preserve the high memory byte 
+   push ax		;on the stack
+   mov al, byte [ds:si]	;preserve the low memory byte
+   push ax		;on the stack
+ 
+   mov byte [fs:si], 0x00	;set high memory to zeros
+   mov byte [ds:di], 0xFF	;set low memory to ones
+   cmp byte [fs:si], 0xFF	;high memory should still be ones
+ 
+   pop ax		;from the stack
+   mov byte [ds:di], al	;restore lower the memory byte
+   pop ax		;from the stack
+   mov byte [fs:si], al	;restore the higher memory byte
+ 
+   je .disable	;jump over the success part if the values were the same
+ 
+   mov ax, 0	;ax=0 means the memory values were different
+   ret   	;as they should have been so no action is needed
+.disable:
+   mov ax, 1	;ax=1 means the read from high memory looped around to
+   ret		;low memory so the A20 line needs enabling
+
+A20_enable:;attempt to activate the A20 line
+   mov ax, 0xffff	
+   mov fs, ax		;set fs to highest segment
+	
+   call A20_check	;check...
+   cmp ax, 0		;does it work
+   je .done1		;yes...great, jump over the rest
+   mov ax, 0x2401	;try enabling by BIOS
+   int 0x15		;idgaf about the retrn value...
+   call A20_check	;check again
+   cmp ax, 0		;does it work
+   je .done2		;yes...great, jump over the rest
+   mov si, gateFail	;no!
+   call print_string	;give up for now
+   jmp $		;lock up..cant be assed
+   ;keyboard and fast methods should be added here later on...
+
    
-;;;;To be continued.....
+.done1:
+   mov si, gateWoop
+   call print_string
+   ret
+.done2:
+   mov si, gateBios
+   call print_string
+   ret
 
 newLine		db `\n\r`, 0
 diskFail 	db 'Disk read failure :-/ please check and restart computer', 0
-
+gateFail	db 'Could not open A20...', 0
+gateWoop	db 'A20 was already open!', 0
+gateBios	db 'A20 was opened by BIOS', 0
+welcome		db 'Kal_OS', 0
 times 510-($-$$) db 0	;null padding macro
 dw 0xaa55		;MBR identifier
 
-;; ######more code here######
+call A20_enable		;enable A20, subroutine will lockup if it fails
 
+call new_line
+mov si, welcome		;print welcome message
+call print_string
 
+jmp $			;nothing else to do at the moment...lockup
+
+;just to move this stuff away so the hex is easier to read.
+times 128 db 0
 
 gdt:;temporary global descriptor table for 32-bit mode
    .null:
@@ -138,6 +175,8 @@ init_PM:
    mov gs, ax
 
    mov esp, 0x7fffe	;place stack ontop of previous stack
-   mov ebp, esp
+   mov ebp, esp		;set base to start of stack
+
+   jmp $		;this jump should be to more code...
 
 times 8192-($-$$) db 0	;null padding macro upto 8k
